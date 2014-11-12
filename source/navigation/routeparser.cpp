8757f592775a7322ecf9f6dbe2e57504b525bda9
MaxReimann <max.reimann@student.hpi.uni-potsdam.de>
2014-11-12 22:01:51 +0100

    added routeparser file

---
+++ source/navigation/routeparser.cpp
@@ -0,0 +1,15 @@
+#include "routeparser.h"
+// osg
+
+#include <osgViewer/View>
+#include <osgViewer/config/SingleWindow>
+#include <osg/ValueObject>
+
+// troen
+#include "../controller/bikecontroller.h"
+
+
+using namespace troen;
+
+
+
---
+++ source/navigation/routeparser.h
@@ -0,0 +1,20 @@
+#pragma once
+
+// OSG
+#include <osgViewer/View>
+#include <osgText/Text>
+
+// troen
+#include "../sampleosgviewer.h"
+#include "../forwarddeclarations.h"
+
+
+namespace troen
+{
+
+	class Route
+	{
+	public:
+
+	};
+}
